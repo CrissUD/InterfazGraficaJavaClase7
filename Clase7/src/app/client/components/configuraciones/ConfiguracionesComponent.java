03d9157b45b40a99e155db864e284946bf264243
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-09-21 00:25:52 -0500

    Arreglos en el código de la clase 7

--- Clase7/src/app/client/components/configuraciones/ConfiguracionesComponent.java
+++ Clase7/src/app/client/components/configuraciones/ConfiguracionesComponent.java
@@ -1,14 +1,14 @@
 package app.client.components.configuraciones;
 
 public class ConfiguracionesComponent {
+  private ConfiguracionesTemplate configuracionesTemplate;
 
-    private ConfiguracionesTemplate configuracionesTemplate;
+  public ConfiguracionesComponent() {
 
-    public ConfiguracionesComponent(){
-        configuracionesTemplate = new ConfiguracionesTemplate(this);
-    }
-    
-    public ConfiguracionesTemplate getConfiguracionesTemplate(){
-        return configuracionesTemplate;
-    }
+    configuracionesTemplate = new ConfiguracionesTemplate(this);
+  }
+
+  public ConfiguracionesTemplate getConfiguracionesTemplate() {
+    return configuracionesTemplate;
+  }
 }
